bf4ea67c55452b63eb70dd4186fe9b1792de86e4
ryancharris <harrisryan1@gmail.com>
2016-06-09 11:59:19 -0400

    Finished Lesson 11

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -33,6 +33,12 @@ var App = React.createClass({
 		this.setState({ fishes: this.state.fishes });
 	},
 
+	loadSamples : function() {
+		this.setState({
+			fishes: require('./sample-fishes')
+		});
+	},
+
 	render : function() {
 		return (
 			<div className="catch-of-the-day">
@@ -40,7 +46,7 @@ var App = React.createClass({
 					<Header tagline="Fresh Seafood Market" />
 				</div>
 				<Order />
-				<Inventory addFish={this.addFish} />
+				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 		)
 	}
@@ -126,6 +132,7 @@ var Inventory = React.createClass({
 			<div>
 				<h2>Inventory</h2>
 				<AddFishForm {...this.props} />
+				<button onClick={this.props.loadSamples}>Load Sample Fishes</button>
 			</div>
 		)
 	}
